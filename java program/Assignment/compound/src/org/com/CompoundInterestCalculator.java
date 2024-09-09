39840a2e3cfacb7d2a41ef453d8519ddba8c90b4
Rasikaspawar <117800653+Rasikaspawar@users.noreply.github.com>
2024-09-09 13:28:47 +0530

    Add files via upload

---
+++ java program/Assignment/compound/src/org/com/CompoundInterestCalculator.java
@@ -0,0 +1,44 @@
+package org.com;
+import java.util.*;
+public class CompoundInterestCalculator {
+	double P;
+	double R;
+	double y;
+	double N;
+	int n;
+    double Fv;
+    double TIE;
+    
+    public void acceptRecord() {
+    	Scanner sc = new Scanner(System.in);
+    	System.out.println("Enter the principle:");
+    	P = sc.nextDouble();
+    	System.out.println("Enter the Rate:");
+    	R = sc.nextDouble();
+    	System.out.println("Enter the year:");
+    	y = sc.nextDouble();
+    	System.out.println("Enter the time:");
+    	N = sc.nextDouble();
+    	
+    }
+    
+    public void calculateFutureValue() {
+    	Fv = P*Math.pow(1+R/N,N*y);
+    	TIE = Fv - P;
+    	
+    }
+    public void printRecord() {
+    	System.out.println("Future value ₹%.2f%n : " +Fv);
+    	System.out.println("Total interset ₹%.2f%n :" +TIE);
+    } 
+     	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CompoundInterestCalculator CI = new CompoundInterestCalculator();
+		CI.acceptRecord();
+		CI.calculateFutureValue();
+		CI.printRecord();
+
+	}
+
+}
